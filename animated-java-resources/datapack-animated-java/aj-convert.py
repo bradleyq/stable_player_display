2e6afe9a58775006c49690ae8f72b0bc0b4c3def
bradleyq <qu.bradley@gmail.com>
2023-06-11 16:36:58 -0700

    update for aj 0.4.2

--- animated-java-resources/datapack-animated-java/aj-convert.py
+++ animated-java-resources/datapack-animated-java/aj-convert.py
@@ -6,7 +6,7 @@ nArgs = len(sys.argv)
 if nArgs < 2:
     print("usage: aj-convert.py [project] [optional:flags]")
     print("available flags:")
-    print("\t-ns=[namespace]\t\tproject namespace. Default 'zzz'")
+    print("\t-ns=[namespace]\t\tinternal project namespace. Default 'zzzzzzzz'")
     print("\t-pn=[playerName]\tplayer skin to use. Default '' no skin, must be set later in game")
     print("\t-s\t\t\tslim model. Default disabled")
     quit()
@@ -19,14 +19,14 @@ TORSO = "torso"
 RLEG = "right_leg"
 LLEG = "left_leg"
 
-    # name, offset, custom model data
+# name, offset, custom model data
 OFFSETS = ((HEAD, 0.0, 1), (RARM, -1024.0, 2), (LARM, -2048.0, 3), (TORSO, -3072.0, 4), (RLEG, -4096.0, 5), (LLEG, -5120.0, 6))
 OFFSETS_S = ((HEAD, 0.0, 1), (RARM, -1024.0, 7), (LARM, -2048.0, 8), (TORSO, -3072.0, 4), (RLEG, -4096.0, 5), (LLEG, -5120.0, 6))
 
 offsets = OFFSETS
 project = sys.argv[1]
 playerName = ""
-namespace = "zzz"
+namespace = "zzzzzzzz"
 
 if nArgs > 2:
     for arg in sys.argv[2:]:
@@ -38,10 +38,10 @@ if nArgs > 2:
             offsets = OFFSETS_S
 
 
-print("running aj-convert on project " + project + " with namespace " + namespace)
+print("running aj-convert on project " + project + " with internal namespace " + namespace)
 
-#construct summon.mcfunction path
-summonPath =  os.path.join(".", "data", project, "functions", "summon.mcfunction")
+# construct summon.mcfunction path
+summonPath =  os.path.join(".", "data", "animated_java", "functions", project, "summon.mcfunction")
 summonResult = ""
 
 # read summon.mcfunction
@@ -72,33 +72,33 @@ summonResult += f.readline()
 
 f.close()
 
-# # write to summon.mcfunction
+# write to summon.mcfunction
 f = open(summonPath, "w")
 f.write(summonResult)
 f.close()
 
 # construct default_as_bone.mcfunction path
-defaultBonePath =  os.path.join(".", "data", namespace + "_" + project + "_internal", "functions", "apply_variant", "default_as_bone.mcfunction")
+defaultBonePath =  os.path.join(".", "data", "animated_java", "functions", project, namespace, "apply_variant", "default", "as_bone.mcfunction")
 defaultBoneResult = ""
 
-# read default_as_bone.mcfunction
+# read default/as_bone.mcfunction
 f = open(defaultBonePath, "r")
 
 for line in f:
     for offsetPair in offsets:
-        if "aj." + project + ".bone." + offsetPair[0] in line:
+        if "[tag=aj." + project + ".bone." + offsetPair[0] + "]" in line:
             line = line.strip()[:-1] + str(offsetPair[2]) + "\n"
     defaultBoneResult += line
 
 f.close()
 
-# # write to default_as_bone.mcfunction
+# write to default/as_bone.mcfunction
 f = open(defaultBonePath, "w")
 f.write(defaultBoneResult)
 f.close()
 
 # construct tree path
-animationPath = os.path.join(".", "data", namespace + "_" + project + "_internal", "functions", "animations")
+animationPath = os.path.join(".", "data", "animated_java", "functions", project, namespace, "animations")
 
 
 for animation in next(os.walk(animationPath))[1]:
@@ -112,7 +112,7 @@ for animation in next(os.walk(animationPath))[1]:
             f = open(animationLeafPath, "r")
             for line in f:
                 for offsetPair in offsets:
-                    if "aj." + project + ".bone." + offsetPair[0] in line:
+                    if "[tag=aj." + project + ".bone." + offsetPair[0] + "]" in line:
                         nbtStart = line.index("merge value ")
                         nbtStart += len("merge value ")
 
